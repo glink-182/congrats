f79c9b2a0530e2591b77545603707f93c3567241
glink-182 <jglinka@st.swps.edu.pl>
2024-06-19 13:13:23 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,54 +1,59 @@
 let confetti = [];
-    let font;
+let font;
 
-    function preload() {
-      font = loadFont('Comic-Sans-MS.ttf');
+function preload() {
+  font = loadFont('Comic-Sans-MS.ttf');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  textFont(font);
+  createConfetti();
+  setTimeout(redirectToSite, 5000); // Redirect after 5 seconds
+}
+
+function draw() {
+  background(255);
+  drawCongratulationsScreen();
+}
+
+function drawCongratulationsScreen() {
+  fill(0);
+  textSize(48);
+  stroke(255, 0, 0);
+  textAlign(CENTER, CENTER);
+  text("Congratulations you made the right choice!!!", width / 2, height / 2);
+
+  drawConfetti();
+}
+
+function createConfetti() {
+  for (let i = 0; i < 200; i++) {
+    confetti.push({
+      x: random(width),
+      y: random(height),
+      length: random(10, 20),
+      angle: random(TWO_PI),
+      speed: random(1, 3),
+      color: color(random(255), random(255), random(255))
+    });
+  }
+}
+
+function drawConfetti() {
+  for (let i = 0; i < confetti.length; i++) {
+    let c = confetti[i];
+    stroke(c.color);
+    strokeWeight(4);
+    line(c.x, c.y, c.x + cos(c.angle) * c.length, c.y + sin(c.angle) * c.length);
+    c.y += c.speed;
+    if (c.y > height) {
+      c.y = 0;
+      c.x = random(width);
     }
+  }
+}
 
-    function setup() {
-      createCanvas(windowWidth, windowHeight);
-      textFont(font);
-      createConfetti();
-    }
-
-    function draw() {
-      background(255);
-      drawCongratulationsScreen();
-    }
-
-    function drawCongratulationsScreen() {
-      fill(0);
-      textSize(48);
-      stroke(255,0,0);
-      textAlign(CENTER, CENTER);
-      text("Congratulations you made the right choice!!!", width / 2, height / 2);
-
-      drawConfetti();
-    }
-
-    function createConfetti() {
-      for (let i = 0; i < 200; i++) {
-        confetti.push({
-          x: random(width),
-          y: random(height),
-          length: random(10, 20),
-          angle: random(TWO_PI),
-          speed: random(1, 3),
-          color: color(random(255), random(255), random(255))
-        });
-      }
-    }
-
-    function drawConfetti() {
-      for (let i = 0; i < confetti.length; i++) {
-        let c = confetti[i];
-        stroke(c.color);
-        strokeWeight(4);
-        line(c.x, c.y, c.x + cos(c.angle) * c.length, c.y + sin(c.angle) * c.length);
-        c.y += c.speed;
-        if (c.y > height) {
-          c.y = 0;
-          c.x = random(width);
-        }
-      }
-    }
+function redirectToSite() {
+  window.location.href = 'https://pmaksymenko.github.io/errornazaliczenie/';
+}
